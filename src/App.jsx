302d6111e9d4770969e8d60a554c4d4e72cad3aa
Arman Siddiqui <20236380@student.westernsydney.edu.au>
2024-05-20 15:47:02 +1000

    Added Customer Complaint Table

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import BarChart from './components/BarChart';
 import PieChart from './components/PieChart';
 import QueueTimer from './components/QueueTimer';
+import ComTable from './components/ComTable';
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -14,6 +15,7 @@ return (
     <Route path ="/LoungeChart" element={<BarChart/>}/>
     <Route path ="/Concen" element={<PieChart/>}/>
     <Route path ="/QueueTime"element={<QueueTimer/>}/>
+    <Route path ="/CustomerComplaint"element={<ComTable/>}/>
     </Routes>
   </Router>
 );
